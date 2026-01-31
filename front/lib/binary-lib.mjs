bfeba4f2493f42de86372161d7c0e9a902ddbd6b
Bakino <contact@bakino.fr>
2026-01-31 20:39:11 +0000

    improve geturl

--- front/lib/binary-lib.mjs
+++ front/lib/binary-lib.mjs
@@ -1,5 +1,13 @@
 export const binaryHelpers = {
     getUrl: (binary) => {
+        if(!binary){
+            // no binary, give transparent image
+            return "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII="
+        }
+        if(binary.data){
+            // data inside the image
+            return `data:${binary.data};base64,${binary.data}` ;
+        }
         return "/open-bamz-binary-storage/binary/" + (binary.index_id??binary) ;
     },
 };
